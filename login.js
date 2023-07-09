b9fa7ffce69562428e0420774732cac5ac6329be
maulidina18 <96989313+maulidina18@users.noreply.github.com>
2023-07-09 23:40:32 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,17 @@
+//alert('login JS');
+
+const correctEmail = 'jwd_b@gmail.com';
+const correctPassword = '1234';
+
+const messageContainer = document.getElementById('message-container');
+
+function cekLogin(){
+    const userEmail = document.getElementById('fEmail').value;
+    const userPassword = document.getElementById('fPassword').value;
+
+    if ((userEmail != correctEmail) || (userPassword != correctPassword)){
+        messageContainer.innerHTML = "<p class='error-message'>Login Gagal, Password atau Email salah !</p>";
+    } else {
+        messageContainer.innerHTML = "<p class='success-message'>Login Berhasil</p>";
+    }
+}
